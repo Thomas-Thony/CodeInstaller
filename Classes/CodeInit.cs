6268e3353da6d9cf97ff291caa525a763dab5580
Thomas.Th <thomas.thony.69@gmail.com>
2026-03-31 11:25:04 +0200

    [NEW] Installation de l'architecture du projet

---
+++ Classes/CodeInit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeInstaller.Classes
+{
+    public class CodeInit{
+        private bool latest = true;
+        private string projectPath;
+
+        public CodeInit(bool? stateVersion, string path) {
+            this.latest = stateVersion ?? true;
+            this.ProjectPath = path;
+        }
+
+        public bool Latest { get => latest; set => latest = value; }
+        public string ProjectPath { get => projectPath; set => projectPath = value; }
+    }
+}
