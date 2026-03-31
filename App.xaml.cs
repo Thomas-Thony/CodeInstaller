865e42994671a719c42a7558a66fe5bffc3b9613
Thomas.Th <thomas.thony.69@gmail.com>
2026-03-31 11:18:17 +0200

    Ajoutez des fichiers projet.

---
+++ App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using CodeInstaller.Screens;
+
+namespace CodeInstaller
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ Screens/AppShell.xaml.cs
@@ -0,0 +1,8 @@
+﻿namespace CodeInstaller.Screens;
+
+public partial class AppShell : Shell
+{
+    public AppShell() {
+        InitializeComponent();
+    }
+}
